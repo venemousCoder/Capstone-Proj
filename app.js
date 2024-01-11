6ac67c7ef184a8478efd4d50c4850200d45b90a6
venemousCoder <ahmadgameriv@gmail.com>
2024-01-11 07:06:16 +0100

    added crud operations to the app. Now you can create, get, uppdate and delete a user.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const ejs = require('ejs');
 const control = require('./controllers/controller.js')
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+
 
 //app and DB settings
 
@@ -17,29 +20,39 @@ mongoose.connect("mongodb://127.0.0.1:27017/spiciesDB", {
 
 //app settings
 
+const router = express.Router()
 const app = express();
 app.set('view engine', ejs);
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use('/', router)
+router.use(methodOverride("_method", {
+    methods: ["POST", "GET"]
+}));
 // app.use((req, res, next) => {
 //     console.table(['Url', req.url, 'Path', req.path, 'Method', req.method, 'Query', req.body.email, 'Status', req.statusCode, 'Message', req.statusMessage])
 //     next()
 // })
 
 //server port
-let port = process.env_PORT || 3000
+let port = process.env_PORT || 8000
 
 
 //routes
 
-app.get('/', control.homeRoute)
-app.get('/login', control.loginRoute)
-app.get('/signin', control.signinRoute)
-app.get('/subs', control.showSubs)
-app.post('/spicies', control.spicesRoute);
-app.get('*', control.errRoute)
-app.post('*', control.errRoute)
+
+router.get('/', control.homeRoute)
+router.get('/login', control.loginRoute)
+router.get('/signin', control.signinRoute)
+router.get('/subs', control.showSubs)
+router.delete('/subs/delete/:id', control.deleteUserM,control.showSubs)
+router.get('/user/:id', control.showUserM, control.showUser)
+router.get('/update/:id', control.updateUserM, control.updateUserGet)
+router.put('/update/:id/', control.updateUserM, control.updateUser,control.showUserM,control.showUser)
+router.get('/spicies',control.spicesRouteGetM, control.spicesRouteGet);
+router.post('/spicies', control.spicesRouteMiddleWare, control.spicesRoute);
+router.all('*', control.errRoute)
 
 
 
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,87 +1,192 @@
-const fs = require('fs');
-const url = require('url');
-const mod = require('../models/myusers');
-const spices = require('../models/spices');
+const fs = require("fs");
+const url = require("url");
+const mod = require("../models/myusers");
+const spices = require("../models/spices");
 
 //getting data from a json file
 
-var items = fs.readFileSync('./public/items.json', 'utf8')
+var items = fs.readFileSync("./public/items.json", "utf8");
 let item = JSON.parse(items);
 
 //middlewares
 
-function showSubs(req, res) {
-    mod.user.find()
-        .then((subs) => {
-            subs.forEach((elm) => {
-                // console.log(elm);
-                elm.populate('spiceCourse')
-                    .then((item) => {
-                        return item.spiceCourse
-                    })
-            })
-            setTimeout(() => {
-                res.render('contact.ejs', { users: subs })
-            }, 300)
-        })
-        .catch(err => console.log(err))
-}
 
-//routes
+//routes controllers
 
 function homeRoute(req, res) {
-    res.render('home.ejs', { 'items': item });
+    res.render("home.ejs", { items: item });
 }
-function courseSignUp(req, res) {
-    spices.spicy.findOne()
+
+//get all users controllers
+
+function showSubs(req, res) {
+    mod.user
+        .find()
+        .then((subs) => {
+            subs.forEach((elm) => {
+                elm.populate("spiceCourse").then((item) => {
+                    return item.spiceCourse;
+                });
+            });
+            setTimeout(() => {
+                res.render("contact.ejs", { users: subs });
+            }, 200);
+        })
+        .catch((err) => console.log(err));
 }
-function spicesRoute(req, res) {
-    mod.user.create({
+
+
+//show a specific user
+
+
+function showUserM(req, res, next) {
+    mod.user
+        .findById(req.params.id)
+        .then((resul) => {
+            resul.populate("spiceCourse").then((resu) => {
+                res.locals.resul = resul;
+                next();
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            next();
+        });
+}
+function showUser(req, res) {
+    res.render("user.ejs", { user: res.locals.resul });
+}
+
+//update a user's profile controllers
+
+function updateUserM(req, res, next) {
+    mod.user
+        .findById(req.params.id)
+        .then((result) => {
+            result
+            res.locals.result = result;
+            next();
+        })
+        .catch((err) => {
+            console.log(err);
+            next();
+        });
+}
+function updateUser(req, res) {
+    var updatedProfile = {
         userName: req.body.username,
         email: req.body.email,
-        subscribed: req.body.subscribe
-    }).then(() => {
-
-        res.render('spicies.ejs',
-            {
-                "username": req.body.username,
-                "email": req.body.email,
-                "subscribed": req.body.subscribe,
-                "spicer": spicer,
-                "error": null
-            }
-        );
-    })
+        subscribed: req.body.subscribe,
+    };
+    mod.user.findByIdAndUpdate(res.locals.result._id, { $set: updatedProfile })
+        .then(() => {
+            res.render("user.ejs", { user: res.locals.result });
+        })
         .catch((err) => {
-            res.render('signin.ejs', { 'error': err || null });
+            res.render("update.ejs", { user: res.locals.result, error: err || null });
+            console.log(err);
         })
 }
-function loginRoute(req, res) {
-    res.render('login.ejs');
-}
-function signinRoute(req, res) {
-    res.render('signin.ejs', { 'error': null });
+function updateUserGet(req, res) {
+    res.render("update.ejs", { user: res.locals.result, error: null });
 }
 
-// error handling route
+//delete controllers
+
+function deleteUserM(req, res) {
+    mod.user.findByIdAndDelete(req.params.id)
+        .then((resu) => {
+            res.redirect("/subs");
+        })
+        .catch((err) => {
+            res.render("home.ejs");
+            console.log(err);
+        })
+}
+
+function deleteUser(req, res) {
+    res.render("contact.ejs", { users: res.locals.result });
+}
+
+
+//other controllers
+function spicesRoute(req, res, next) {
+    res.redirect(res.locals.redirect);
+    next();
+}
+
+function spicesRouteGetM(req, res, next) {
+    spices.spicy.find()
+        .then((spicer) => {
+            res.locals.spicer = spicer
+            next()
+        })
+        .catch((err) => {
+            console.log(err)
+            next()
+        })
+}
+
+function spicesRouteGet(req, res,) {
+    res.render('spicies.ejs', { spicer: res.locals.spicer });
+}
+
+function spicesRouteMiddleWare(req, res, next) {
+    mod.user
+        .create({
+            userName: req.body.username,
+            email: req.body.email,
+            subscribed: req.body.subscribe,
+        })
+        .then(() => {
+            res.locals.redirect = "/subs";
+            next();
+        })
+        .catch((err) => {
+            res.render("signin.ejs", { error: err || null });
+            next();
+        });
+}
+
+function loginRoute(req, res) {
+    res.render("login.ejs");
+}
+
+function signinRoute(req, res) {
+    res.render("signin.ejs", { error: null });
+}
+
+// error handling controller
 
 function errRoute(req, res) {
     if (res.statusCode !== 404) {
-        res.render('error.ejs',
-            {
-                "error": res.statusCode,
-                "error1": res.statusMesssage,
-            }
-        )
+        res.render("error.ejs", {
+            error: res.statusCode,
+            error1: res.statusMesssage,
+        });
     } else {
-        res.render('error.ejs',
-            {
-                "error": res.statusCode = 404,
-                "error1": res.statusMesssage,
-            }
-        )
-
+        res.render("error.ejs", {
+            error: (res.statusCode = 404),
+            error1: res.statusMesssage,
+        });
     }
 }
 
-module.exports = { signinRoute, spicesRoute, loginRoute, homeRoute, errRoute, showSubs, courseSignUp }
+module.exports = {
+    signinRoute,
+    spicesRoute,
+    spicesRouteGetM,
+    spicesRouteGet,
+    spicesRouteMiddleWare,
+    loginRoute,
+    homeRoute,
+    errRoute,
+    showSubs,
+    showUserM,
+    showUser,
+    updateUserM,
+    updateUser,
+    updateUserGet,
+    deleteUserM,
+    deleteUser,
+};
